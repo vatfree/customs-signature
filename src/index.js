5ab224672a5d00f4cfbc1ed6e66091fc3472c846
Jaco Swarts <jaco@vatfree.com>
2024-04-17 09:18:53 +0200

    Remove standard output from testing results

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import { readFile } from "fs/promises"
 import { X509Certificate, createHash, createVerify } from "crypto"
 
 function cultureInvariantFloat(num) {
-	// const result = Number(num).toFixed(2)
-	// return result.replace(/\.(\d)0/, ".$1")
 	const roundedValue = Math.round(+num * 100) / 100 // Round to 2 decimal places
-
 	// Convert to string and remove second trailing zero if present
 	const stringValue = Number.isInteger(roundedValue * 10)
 		? roundedValue.toFixed(1)
 		: roundedValue.toFixed(2)
-
 	return stringValue
 }
 
@@ -18,22 +14,17 @@ function getMessage(validation) {
 	return `${validation.ValidationRequestId}${validation.Approved}${validation.ReasonNotApproved || ""}${validation.ReasonNotApprovedCode}${cultureInvariantFloat(validation.TotalValue)}${cultureInvariantFloat(validation.TotalVat)}`
 }
 
-export default async function validateSignature({ path }) {
+export default async function verifySignature({ path }) {
 	// read a text file
 	const fileContents = await readFile(path, { encoding: "utf8" })
-
 	// deserialize text into an object
 	const request = JSON.parse(fileContents)
-
 	// convert from base64 text into a public key
 	const publicKeyBuffer = Buffer.from(request.PublicKey, "base64")
-
 	// create a X509 certificate from public key
 	const certificate = new X509Certificate(publicKeyBuffer)
-
 	// should get the RSA public key from the certificate
 	const rsaPublicKey = certificate.publicKey
-
 	const verifyResults = []
 	for (let validationResult of request.ValidationResults) {
 		// convert from base64 text the signature of proof
@@ -66,29 +57,32 @@ export default async function validateSignature({ path }) {
 	}
 }
 
-async function main() {
-	const logSummary = ({ verifyResults }) =>
-		verifyResults.forEach(({ id, isValid }) =>
+function logSummary({ verifyResults }) {
+	verifyResults.forEach(
+		({ id, isValid }) =>
+			process.env.npm_lifecycle_event !== "test" &&
 			console.log(`-- Signature of result ${id} is ${isValid ? "valid" : "invalid"}`)
-		)
+	)
+}
 
+async function main() {
 	logSummary(
-		await validateSignature({
+		await verifySignature({
 			path: "assets/IN-CU005/request.json",
 		})
-	) // `-- Signature of result isValid))
+	)
 	logSummary(
-		await validateSignature({
+		await verifySignature({
 			path: "assets/IN-CU006/request.json",
 		})
 	)
 	logSummary(
-		await validateSignature({
+		await verifySignature({
 			path: "assets/IN-CU007/request.json",
 		})
 	)
 	logSummary(
-		await validateSignature({
+		await verifySignature({
 			path: "assets/IN-CU008/request.json",
 		})
 	)
